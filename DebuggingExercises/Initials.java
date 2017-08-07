9d3defc774d9f6f9bfb16084bffc374f7b32da59
mitch <mitchell.cairney@my.jcu.edu.au>
2017-08-07 12:20:08 +1000

    made a new class

---
+++ DebuggingExercises/Initials.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Jc260799 on 7/08/17.
+ */
+public class Initials {
+
+    public static void main(String[] args) {
+        char firstInitial = 'M';
+        char secondInitial = 'R';
+        char thirdInitial = 'C';
+
+        System.out.println(firstInitial+"."+secondInitial+"."+thirdInitial+".");
+    }
+}
